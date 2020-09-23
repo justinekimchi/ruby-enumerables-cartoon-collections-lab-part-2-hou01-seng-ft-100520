8b1a39706198724d9359a6a2d8214e87b21e9c03
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-23 04:46:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |value|
-    [valid_calls]&[planeteer_calls] == value
+   # [valid_calls]&[planeteer_calls] == value
     #valid_calls.include?()
   
     end
